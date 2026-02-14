108176a54c80ea825a017ed0cd3938ad8d67020a
Fhmi-srj <msirojul508@gmail.com>
2026-02-14 19:17:36 +0700

    fix: make scheduler robust against missing app_settings table

--- routes/console.php
+++ routes/console.php
@@ -20,20 +20,44 @@ Schedule::command('wa:absen-reminder')->everyMinute()
 
 // Group: Jadwal harian
 Schedule::command('wa:schedule-daily')
-    ->dailyAt(\App\Models\AppSetting::getValue('wa_schedule_time', config('services.mpwa.schedule_time', '06:30')))
+    ->dailyAt((function() {
+        try {
+            return \App\Models\AppSetting::getValue('wa_schedule_time', config('services.mpwa.schedule_time', '06:30'));
+        } catch (\Exception $e) {
+            return config('services.mpwa.schedule_time', '06:30');
+        }
+    })())
     ->appendOutputTo(storage_path('logs/whatsapp-scheduler.log'));
 
 // Group: Rekap absensi mengajar
 Schedule::command('wa:attendance-recap')
-    ->dailyAt(\App\Models\AppSetting::getValue('wa_recap_time', config('services.mpwa.recap_time', '13:30')))
+    ->dailyAt((function() {
+        try {
+            return \App\Models\AppSetting::getValue('wa_recap_time', config('services.mpwa.recap_time', '13:30'));
+        } catch (\Exception $e) {
+            return config('services.mpwa.recap_time', '13:30');
+        }
+    })())
     ->appendOutputTo(storage_path('logs/whatsapp-scheduler.log'));
 
 // Group: Laporan kegiatan & rapat
 Schedule::command('wa:activity-report')
-    ->dailyAt(\App\Models\AppSetting::getValue('wa_activity_report_time', config('services.mpwa.activity_report_time', '18:00')))
+    ->dailyAt((function() {
+        try {
+            return \App\Models\AppSetting::getValue('wa_activity_report_time', config('services.mpwa.activity_report_time', '18:00'));
+        } catch (\Exception $e) {
+            return config('services.mpwa.activity_report_time', '18:00');
+        }
+    })())
     ->appendOutputTo(storage_path('logs/whatsapp-scheduler.log'));
 
 // Group: Undangan rapat H-2 + Pengingat H
 Schedule::command('wa:meeting-invitation')
-    ->dailyAt(\App\Models\AppSetting::getValue('wa_meeting_invite_time', config('services.mpwa.meeting_invite_time', '07:00')))
+    ->dailyAt((function() {
+        try {
+            return \App\Models\AppSetting::getValue('wa_meeting_invite_time', config('services.mpwa.meeting_invite_time', '07:00'));
+        } catch (\Exception $e) {
+            return config('services.mpwa.meeting_invite_time', '07:00');
+        }
+    })())
     ->appendOutputTo(storage_path('logs/whatsapp-scheduler.log'));
